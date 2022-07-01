26d92c7cc8199d069ebef36de48608bc875079db
NJMT5263 <kevin.battais@orange.com>
2022-07-01 15:24:23 +0200

    z

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Employee;
 import com.example.demo.dto.Employees;
 import com.example.demo.services.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +29,17 @@ public class DemoController {
         return ResponseEntity.ok(employees);
     }
 
+    @GetMapping(value = "/employee")
+    public ResponseEntity<Employees> getEmployee(@RequestParam(value = "employeeId") String employeeId) {
+        //TODO Recupère un employé
+        return null;
+    }
+
+
+    @PostMapping(value = "/employee")
+    public ResponseEntity<Employees> postEmployee(@RequestBody Employee employee) {
+        //TODO Creation d'un employé
+        return null;
+    }
 
 }
